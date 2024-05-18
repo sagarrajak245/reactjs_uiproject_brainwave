23df4d482e20b598d729393d3f6e873b5f05b705
sagar rajak <sagarrajak245@gmail.com>
2024-05-18 13:29:12 +0530

    my firstcommit

---
+++ src/components/Heading.jsx
@@ -0,0 +1,28 @@
+import TagLine from "./Tagline";
+
+const Heading = ({ className, title, text, tag }) => {
+  return (
+    <div
+      className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
+    >
+      {tag && <TagLine className="mb-4 md:justify-center">{tag}</TagLine>}
+      {title && <h2 className="h2">{title}</h2>}
+      {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
+    </div>
+  );
+};
+
+export default Heading;
+// import React from "react";
+
+// function Heading({ className, title }) {
+//   return (
+//     <div
+//       className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
+//     >
+//       {title && <h2 className="h2">{title}</h2>}
+//     </div>
+//   );
+// }
+
+// export default Heading;
